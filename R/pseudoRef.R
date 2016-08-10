1ad6d5885003cfacf160bddfc7fee1f97601b330
Jinliang Yang <yangjl0930@gmail.com>
2016-08-10 10:54:38 -0700

    updated main function. v0.1.1

--- R/pseudoRef.R
+++ R/pseudoRef.R
@@ -1,30 +1,50 @@
 #' \code{pseudoRef}
 #' Make a pseudo reference genome.
 #'
-#' @param fa Path for the reference fasta file. [string]
+#' @param fa Path for the reference fasta file. [string or Robj: DNAStringSet]
 #' @param snpdt A data.table object with heterozygote SNPs coded with IUPAC ambiguity codes. [data.table]
 #' @param sidx A vector to indicate the sample columns. [vector].
-#' @param srules Nucleotide substitution rules defined by users. [data.frame]
-#'                  For example, sub_rules <- data.frame(from=c("M", "Y", "R", "K"), to=c("C", "C", "G", "T")).
-#' @param dir Output directory. Sample specific sub-folders will be created. [string]
+#' @param arules Additional nucleotide substitution rules defined by users. [data.frame:from,to]
+#'               For example, sub_rules <- data.frame(from=c("M", "Y", "R", "K"), to=c("C", "C", "G", "T")).
+#' @param outdir Output directory. Sample specific sub-folders will be created. [string]
 #'
 #' @return Fasta files in specified directory under sample-specific sub-folders. [fasta].
 #'
 #' @examples
-#' sub_rules <- data.frame(from=c("M", "Y", "R", "K"), to=c("C", "C", "G", "T"))
+#' # First of all, use BCFtools to convert VCF into IUPAC coded data.table:
+#'
+#' # bcftools view JRI20_filtered_snps_annot.bcf.gz -m2 -M2 -v snps -Oz -o JRI20_bi_snps_annot.vcf.gz
+#' # bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%IUPACGT]\n' JRI20_bi_snps_annot.vcf.gz > JRI20_bi_snps_annot.txt
+#' # bcftools query -f 'chr\tpos\tref\talt[\t%SAMPLE]\n' JRI20_bi_snps_annot.vcf.gz > JRI20_bi_snps_annot.header
+#'
+#' arules <- data.frame(from=c("M", "Y", "R", "K"), to=c("C", "C", "G", "T"))
 #'
 #' features <- c("C methylated in CHH context")
 #' get_file2tab(files, features, replace=T )
 #'
 #' @export
-pseudoRef <- function(fa, snpdt, sidx=5:24, srules){
+pseudoRef <- function(fa, snpdt, sidx=5:24, arules, outdir){
+
+  ### load reference genome fasta file into DNAStringSet
+  library("Biostrings")
+  if(class(fa) == "character"){fa <- readDNAStringSet(filepath = fa, format="fasta")}
+  if(class(fa) != "DNAStringSet" & class(fa) != "DNAString"){stop("fa should be a DNAStringSet or DNAString")}
+  if(sum(names(arules) %in% c("from", "to")) !=2)
+    {stop("arules should be a data.frame with at least two columns of from and to")}
+  tab0 <- alphabetFrequency(fa)
+  wd0 <- width(fa)
+  nm0 <- names(fa)
+
+  ### chr id, get only the first element of the blank seperated vector.
   chrid <-  gsub(" .*", "", names(fa))
+  res <- list()
+  ### substitute one sample at a time
+  for(s in sidx){
+    myfa <- fa
+    #tab0 <- table(snpdt[, s, with=FALSE])
 
-  ### get one sample at a time
-  for(s in sampleidx){
-
-    sub <- snpdf[, c(1:4, s), with=FALSE]
-    sid <- names(snpdf)[s]
+    sub <- snpdt[, c(1:4, s), with=FALSE]
+    sid <- names(snpdt)[s]
     names(sub)[5] <- "SAMPLE"
     sub <- sub[SAMPLE != "./." & ref != SAMPLE]
 
@@ -34,15 +54,28 @@ pseudoRef <- function(fa, snpdt, sidx=5:24, srules){
       ## remove missing sites and sites that are the same as ref
       idx <- which(chrid[i] == chrid)
 
-      ## replaced base-pairs based on sub_rules
-      for(k in 1:nrow(sub_rules)){
-        subchr[SAMPLE == sub_rules$from[k], SAMPLE := sub_rules$to[k]]
+      ## replaced base-pairs according to arules
+      for(k in 1:nrow(arules)){
+        subchr[SAMPLE == arules$from[k], SAMPLE := arules$to[k]]
       }
 
-      fa[[idx]] <- replaceLetterAt(x=fa[[idx]], at=subchr[, pos], letter=subchr[, SAMPLE],
+      myfa[[idx]] <- replaceLetterAt(x=myfa[[idx]], at=subchr[, pos], letter=subchr[, SAMPLE],
                                    if.not.extending="replace", verbose=FALSE)
     }
 
     ### write for each sample
+    tab1 <- alphabetFrequency(myfa)
+    wd1 <- width(myfa)
+    nm1 <- names(myfa)
+    if(sum(wd0 - wd1) != 0 & sum(nm0 != nm1) != 0)
+      {stop(sprintf("!!! changed chr length or chr id for sample [ %s ]", sid))}
+
+    dir.create(paste0(outdir, "/", sid), showWarnings = FALSE)
+    message(sprintf("###>>> printing fasta file for sample: [ %s ] ...", sid))
+    writeXStringSet(myfa, paste0(outdir, "/", sid, "/", sid, ".fasta"), format="fasta", append=FALSE)
+
+    ###output reprot
+    res[[sid]] <- tab1 - tab0
   }
+  return(res)
 }
